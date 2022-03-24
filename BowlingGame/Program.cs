082f263120cf762c79ff0c109d5a542d1083f336
Bigeard <robin.bigeard@gmail.com>
2022-03-24 09:54:57 +0100

    je te renvoie la chance

--- BowlingGame/Program.cs
+++ BowlingGame/Program.cs
@@ -4,9 +4,29 @@ namespace BowlingGame
 {
     public class Game
     {
-        public uint Draw()
+        public void Draw()
         {
-            return 0;
+            for (int j = 0; j < 10; j++)
+            {
+                int pin = 10;
+                Console.WriteLine(NumberPin(pin));
+            }
+        }
+
+        public int NumberPin(int NumberOfPin)
+        {
+            return new Random().Next(0, NumberOfPin);
         }
     }
+
+    class Program
+    {
+        static void Main()
+        {
+            var game = new BowlingGame.Game();
+            game.Draw();
+        }
+    }
+
 }
+
